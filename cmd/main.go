ef4baae0491d10dbb7af4d47496518cd2feb58dd
PNYwise <bunayya12345@gmail.com>
2024-04-18 13:53:33 +0700

    bug fix

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/PNYwise/graha-migration-tool/internal"
 	"github.com/PNYwise/graha-migration-tool/internal/helper"
-	"gorm.io/gorm"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
@@ -30,37 +29,58 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	categoryRepository := internal.NewCategoryRepository(internal.DB.Db)
+	brandRepository := internal.NewBrandRepository(internal.DB.Db)
+	productRepository := internal.NewProductRepository(internal.DB.Db)
+
 	fmt.Printf("Get data from DB \n")
-	dbCategories := getCategoryFromDb(internal.DB.Db)
-	dbBrands := getBrandFromDb(internal.DB.Db)
-	dbProducts := getProductFromDb(internal.DB.Db)
+	dbCategories, err := categoryRepository.FindAll()
+	if err != nil {
+		log.Fatalf("Err get brands: %v", err)
+	}
+	dbBrands, err := brandRepository.FindAll()
+	if err != nil {
+		log.Fatalf("Err get brands: %v", err)
+	}
+	dbProducts, err := productRepository.FindAll()
+	if err != nil {
+		log.Fatalf("Err get products: %v", err)
+	}
 
 	fmt.Printf("Get data from xls \n")
 	xlsxCategories := getCategoryFromXlsx(xlsx)
 	xlsxBrands := getBrandFromXlsx(xlsx)
-	xlsxProducts := getProductFromXlsx(xlsx, *dbBrands, *dbCategories)
 
 	fmt.Printf("filter data \n")
 	filteredbrands := helper.FilterBrandsNotInByCode(xlsxBrands, *dbBrands)
 	filteredCategories := helper.FilterCategoriesNotInByCode(xlsxCategories, *dbCategories)
-	filteredProducts := helper.FilterProductsNotInByCode(xlsxProducts, *dbProducts)
 
 	fmt.Printf("store data \n")
-	categoryRepository := internal.NewCategoryRepository(internal.DB.Db)
-	brandRepository := internal.NewBrandRepository(internal.DB.Db)
-	productRepository := internal.NewProductRepository(internal.DB.Db)
-	if err := categoryRepository.CreateBatch(filteredCategories);err!= nil {
-		log.Fatalf("error storing category: %v",err)
+
+	if err := categoryRepository.CreateBatch(filteredCategories); err != nil {
+		log.Fatalf("error storing category: %v", err)
 	}
-	if err := brandRepository.CreateBatch(filteredbrands);err!= nil {
-		log.Fatalf("error storing brand: %v",err)
+	if err := brandRepository.CreateBatch(filteredbrands); err != nil {
+		log.Fatalf("error storing brand: %v", err)
 	}
-	if err := productRepository.CreateBatch(filteredProducts);err!= nil {
-		log.Fatalf("error storing product: %v",err)
-	}
-	fmt.Printf("stored Product %v \n", len(filteredProducts))
 	fmt.Printf("stored Brand %v \n", len(filteredbrands))
 	fmt.Printf("stored Category %v \n", len(filteredCategories))
+
+	dbCategories, err = categoryRepository.FindAll()
+	if err != nil {
+		log.Fatalf("Err get brands: %v", err)
+	}
+	dbBrands, err = brandRepository.FindAll()
+	if err != nil {
+		log.Fatalf("Err get brands: %v", err)
+	}
+	xlsxProducts := getProductFromXlsx(xlsx, *dbBrands, *dbCategories)
+	filteredProducts := helper.FilterProductsNotInByCode(xlsxProducts, *dbProducts)
+	if err := productRepository.CreateBatch(filteredProducts); err != nil {
+		log.Fatalf("error storing product: %v", err)
+	}
+	fmt.Printf("stored Product %v \n", len(filteredProducts))
+
 }
 
 func getCategoryFromXlsx(xlsx *excelize.File) []internal.CategoryEntity {
@@ -142,33 +162,6 @@ func getProductFromXlsx(xlsx *excelize.File, brands []internal.BrandEntity, cate
 			}
 			products = append(products, product)
 		}
-	}
-	return products
-}
-
-func getCategoryFromDb(db *gorm.DB) *[]internal.CategoryEntity {
-	categoryRepository := internal.NewCategoryRepository(db)
-	categories, err := categoryRepository.FindAll()
-	if err != nil {
-		log.Fatalf("Err get brands: %v", categories)
-	}
-	return categories
-}
-
-func getBrandFromDb(db *gorm.DB) *[]internal.BrandEntity {
-	brandRepository := internal.NewBrandRepository(db)
-	brands, err := brandRepository.FindAll()
-	if err != nil {
-		log.Fatalf("Err get brands: %v", brands)
-	}
-	return brands
-}
-
-func getProductFromDb(db *gorm.DB) *[]internal.ProductEntity {
-	productRepository := internal.NewProductRepository(db)
-	products, err := productRepository.FindAll()
-	if err != nil {
-		log.Fatalf("Err get products: %v", products)
 	}
 	return products
 }
